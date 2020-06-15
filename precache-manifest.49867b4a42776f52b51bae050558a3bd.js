0d0f7730bbe7f93d218257d1a5c9df5e1875f5fd
Alexandra Feldman <a.n.feldman@student.tudelft.nl>
2020-06-15 15:44:04 +0200

    Updates

---
+++ precache-manifest.49867b4a42776f52b51bae050558a3bd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d2e493af1cd5141661ae6dcdc83008",
+    "url": "/iris/index.html"
+  },
+  {
+    "revision": "bd46e81eb0b1da9a1346",
+    "url": "/iris/static/css/main.36e62ff4.chunk.css"
+  },
+  {
+    "revision": "d5a8be60d4b7184f92c7",
+    "url": "/iris/static/js/2.515d5c7b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/iris/static/js/2.515d5c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd46e81eb0b1da9a1346",
+    "url": "/iris/static/js/main.9f1a6bf6.chunk.js"
+  },
+  {
+    "revision": "1db1d84dec0072fd4e5b",
+    "url": "/iris/static/js/runtime-main.a706ef68.js"
+  },
+  {
+    "revision": "ffd692718e77cea165c3ba364a239ad7",
+    "url": "/iris/static/media/alex-pic.ffd69271.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/iris/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "1cfaeddac4d8a2dbc7c6e40fbfedf744",
+    "url": "/iris/static/media/dani-pic.1cfaedda.png"
+  },
+  {
+    "revision": "dd0392982851ff0170e1df2e97052043",
+    "url": "/iris/static/media/features-split-image-01.dd039298.png"
+  },
+  {
+    "revision": "99e33baa213ce5a30bb36681c483c64b",
+    "url": "/iris/static/media/features-split-image-02.99e33baa.png"
+  },
+  {
+    "revision": "279e820ed0ee4c0c2e90cb52c17b5954",
+    "url": "/iris/static/media/features-split-image-03.279e820e.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/iris/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/iris/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "f1e4a21d4ffda4ac17b8e0219e99ddbf",
+    "url": "/iris/static/media/jean-pic.f1e4a21d.png"
+  },
+  {
+    "revision": "a884de5987f92019c75031cd96a58f81",
+    "url": "/iris/static/media/logo_1000.a884de59.png"
+  },
+  {
+    "revision": "a4563df7a893c87dd0deed0154fd2897",
+    "url": "/iris/static/media/marin-pic.a4563df7.png"
+  },
+  {
+    "revision": "e4306b435beaa55928deaff41633727a",
+    "url": "/iris/static/media/video-placeholder.e4306b43.png"
+  }
+]);
