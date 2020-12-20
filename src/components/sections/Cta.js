049f8b8ec4cc92019e0c689b59044e44017563e2
Jean Gal <j.c.r.gal@student.tudelft.nl>
2020-12-20 17:34:13 +0100

    trying console log on press enter key event

--- src/components/sections/Cta.js
+++ src/components/sections/Cta.js
@@ -41,6 +41,8 @@ const Cta = ({
     bottomDivider && 'has-bottom-divider',
     split && 'cta-split'
   );  
+  
+  const mail = document.querySelector("#newsletter");
 
   return (
     <section
@@ -53,11 +55,11 @@ const Cta = ({
         >
           <div className="cta-slogan">
             <h3 className="m-0 text_email" >
-              Let us your email to get updates
-              </h3>
+              Join our newsletter and keep updated !
+             </h3>
           </div>
           <div className="cta-action">
-            <Input id="newsletter" type="email" label="Subscribe" labelHidden hasIcon="right" placeholder="Your email">
+            <Input id="newsletter" type="email" labelHidden hasIcon="right" placeholder="Your email" onKeyPress={console.log("ma bite")}>
               <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z" fill="#376DF9" />
               </svg>
@@ -67,6 +69,10 @@ const Cta = ({
       </div>
     </section>
   );
+
+  mail.onKeyPress(this.onKeyUp, function() {
+    console.log('I was triggered during componentDidMount') 
+  });
 }
 
 Cta.propTypes = propTypes;
